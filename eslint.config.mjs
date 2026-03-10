b086f1fdb1c2583fdc80a3502b651bb9b955034d
Marsel Melisov <m.melisov@emcd.io>
2026-03-10 22:43:58 +0600

    chore: init monorepo with pnpm workspaces + changesets

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  prettier,
+  {
+    ignores: ['node_modules/', 'dist/', '**/dist/'],
+  },
+];
